f9012904f56cb4e697f03357632793bf51d27450
Wood <j_wood43@fanshawec.ca>
2017-09-29 10:18:01 -0400

    New file

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+$(document).foundation();
+
+(function() {
+	"use strict";
+	console.log("SEAF fired");
+
+
+//Variables
+
+
+
+
+//Functions
+
+
+
+
+
+
+//Listeners
+
+})();
